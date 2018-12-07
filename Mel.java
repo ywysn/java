1b73b57bbe5fc217199076e2a9e67a6509d75dd9
ywysn <1454836216@qq.com>
2018-12-07 20:45:26 +0800

    创建一个Test类，包含有一个public权限的int型成员变量与一个char类型的成员变量，观察在main方法中的初始值。

---
+++ Mel.java
@@ -0,0 +1,10 @@
+class Test{
+    public int a;
+    public char b;
+}
+public class Mel {
+    public static void main(String[] args){
+    Test tes = new Test();
+    System.out.println("a="+tes.a+",b="+tes.b);
+    }
+}
