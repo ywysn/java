f4d5c6a3345b9dc635773d31c36b8f2b6a52d8ae
ywysn <1454836216@qq.com>
2019-02-28 22:23:17 +0800

    恶汉式单例

---
+++ A10.java
@@ -0,0 +1,19 @@
+package www.bit.java;
+class Singal{
+    private static Singal singal = new Singal();
+    private Singal(){
+    }
+    public static Singal getInstance(){
+        return singal;
+    }
+    public void print(){
+        System.out.println("恶汉式单例");
+    }
+}
+public class A10 {
+    public static void main(String[] args) {
+        Singal singal = null;
+        singal = Singal.getInstance();
+        singal.print();
+    }
+}
